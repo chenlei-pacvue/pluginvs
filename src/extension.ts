e3c8714554e86cf91a50f5e59a51622adb0b78c4
chenglie33 <394699890@qq.com>
2022-04-26 19:56:30 +0800

    出版

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { readConfigFile, getConfigByKey,getLangJson } from './config';
-import { addi18n } from './i18n/I18nCommon'
+import { readConfigFile, getConfigByKey, getLangJson } from './config';
+import { getWebviewContent, getWebviewExport } from './webview/webview';
+import { addi18n } from './i18n/I18nCommon'；
+import {FileClass} from './util/FileClass'
 import {
 	WebviewView,
 	WebviewViewResolveContext,
@@ -16,7 +18,7 @@ import * as path from 'path';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+
 	if (!readConfigFile()) {
 		return;
 	}
@@ -27,21 +29,21 @@ export function activate(context: vscode.ExtensionContext) {
 	let langFile = vscode.workspace.workspaceFolders ? `${vscode.workspace.workspaceFolders[0].uri.fsPath}/${langDist}/${useLang}.js` : ''
 	const langmap = getLangJson(path.resolve(langFile));
 	global.langmap = langmap;
-	let MapforValue:any = {};
+	let MapforValue: any = {};
 	for (let key in langmap) {
 		MapforValue[langmap[key]] = key;
 	}
 	console.log('Congratulations, your extension "pacvueextension" is now active!!');
 	addi18n()
 	vscode.languages.registerHoverProvider(['javascript', 'vue'], {
-		provideHover(doc: vscode.TextDocument,position: Position) {
+		provideHover(doc: vscode.TextDocument, position: Position) {
 			// console.log(window.activeTextEditor?.selections[0]);
 			let text = window.activeTextEditor?.selections[0];
 			if (text) {
 				let range = new Range(text.start, text.end)
 				let wenan = window.activeTextEditor?.document.getText(new Range(text.start, text.end))
 				if (!MapforValue[wenan]) return;
-				const params = encodeURIComponent(JSON.stringify({ range: range, text: MapforValue[wenan]}))
+				const params = encodeURIComponent(JSON.stringify({ range: range, text: MapforValue[wenan] }))
 				let details = `发现可以替换的文案：[${MapforValue[wenan]}](command:pacvueextension.replace?${params})`
 				const markdown = new MarkdownString(details, true);
 				markdown.supportHtml = true;
@@ -60,13 +62,13 @@ export function activate(context: vscode.ExtensionContext) {
 			window.activeTextEditor.document.uri,
 			new Range(x.range[0], x.range[1]),
 			`$t('${x.text}')`
-		  );
-		  try {
+		);
+		try {
 			vscode.workspace.applyEdit(edit)
-		  } catch (error) {
-			  console.log(error)
-		  }
-		 
+		} catch (error) {
+			console.log(error)
+		}
+
 		vscode.window.showInformationMessage('Hello World from pacvueExtension!!');
 	});
 	let i18nView = vscode.commands.registerCommand('pacvueextension.show18Tool', () => {
@@ -86,22 +88,40 @@ export function activate(context: vscode.ExtensionContext) {
 		panel.webview.onDidReceiveMessage(
 			message => {
 				switch (message.command) {
-					case 'alert':
-						vscode.window.showErrorMessage(message.text);
+					case 'export':
+						
+						console.log(message)
+						// vscode.window.showErrorMessage(message.text);
+						try {
+							let ff = new FileClass(message.text.file, message.text.outFile, 'B')
+							ff.checkSheet([message.text.sheetname], ["C", "D"])
+							vscode.window.showInformationMessage('导出完成');
+						} catch (error) {
+							console.log(error)
+						}
 						return;
+					case 'selectfile':
+						window.showOpenDialog({canSelectFolders: true, canSelectFiles: false}).then(iten=> {
+						
+							panel.webview.postMessage({ command: 'refactor', text: iten[0].path});
+						})
+						break;
 				}
 			},
 			undefined,
 			context.subscriptions
 		);
-		panel.webview.html = getWebviewContent()
+		panel.webview.html = getWebviewExport()
 
 
 	});
-	
+	let exportExcel = vscode.commands.registerCommand('pacvueextension.exportExcel', (x) => {
+		console.log(x)
+	});
 	context.subscriptions.push(i18nView);
 	context.subscriptions.push(i18nreplace);
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(exportExcel);
 	// context.subscriptions.push(disposables);
 	var thisProvider = {
 		resolveWebviewView: function (thisWebview: WebviewView, contexts: WebviewViewResolveContext) {
@@ -126,61 +146,19 @@ export function activate(context: vscode.ExtensionContext) {
 	);
 	context.subscriptions.push(
 		vscode.window.onDidChangeActiveTextEditor(editor => {
-		  if (editor) {
-			addi18n();
-		  }
+			if (editor) {
+				addi18n();
+			}
 		}, null)
-	  );
+	);
 
 	context.subscriptions.push(
-	vscode.workspace.onDidSaveTextDocument(() => {
-		addi18n()
-	}, null)
+		vscode.workspace.onDidSaveTextDocument(() => {
+			addi18n()
+		}, null)
 	);
 }
-function getWebviewContent() {
-	return `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-	  <meta charset="UTF-8">
-	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	  <title>Cat Coding</title>
-  </head>
-  <style>
-  .btn {
-	align-self: center;
-	margin-top: 10px;
-	max-width: 300px;
-	width: 100%;
-    background-color: cornflowerblue;
-	padding: 12px;
-	border-radius:4px;
-	cursor: pointer;
-	
-}
 
-  </style>
-  <body style='padding-top:20px'>
-  <div class='btn' data-action="pacvueextension.show18Tool">国际化工具页面1</div>
-  <script>
-        (function() {
-            const vscode = acquireVsCodeApi();
-			document.addEventListener('click',function(e) {
-				if (e.target.matches('[data-action]')) {
-					vscode.postMessage({
-						command: 'alert',
-						text:e.target.dataset.action
-					})
-				}
-			})
-            
-        }())
-    </script>
-
-  </body>
-
-  </html>`;
-}
 
 // this method is called when your extension is deactivated
 export function deactivate() { }
---
+++ src/util/FileClass.ts
@@ -0,0 +1,179 @@
+const XLSX = require("xlsx-style");
+
+var fs = require("fs");
+// import { MessageCenter } from "utiltool-pubsub";
+// let message = new MessageCenter();
+class FileClass {
+  repeatkey;
+  regValue;
+  filetype;
+  filename;
+  outfilePath;
+  keycell;
+  workbook;
+  worksheet;
+  fillObj;
+  ExcelProp;
+  xName;
+  constructor(filename, outfilePath, keycell) {
+    this.repeatkey = [];
+    this.regValue = "&";
+    this.filetype = "js";
+    this.filename = filename;
+    this.outfilePath = outfilePath;
+    this.keycell = keycell;
+    this.workbook = XLSX.readFile(filename);
+    this.worksheet = null;
+    this.fillObj = {}; // 记录所有的属性当前的sheet结束与开始
+    this.ExcelProp = []; // [a,1,aa,100]
+    this.xName = ""; // 导出的文件名称
+  }
+  setregValue(v) {
+    this.regValue = v;
+  }
+  setoutPath(v) {
+    this.outfilePath = v;
+  }
+  setkeycell(v) {
+    this.keycell = v;
+  }
+  checkAllSheet() {
+    this.repeatkey = [];
+    this.workbook.SheetNames.forEach(item => {
+      this.worksheet = this.workbook.Sheets[item];
+      this.ExcelProp = this.getExcelProp(this.worksheet["!ref"]);
+      this.fillObj = this.fillColumKey(this.ExcelProp);
+      this.fillObj.layoutX.forEach(items => {
+        this.xName = this.readExcelColumKey(items + 1, item);
+        // if (this.xName.indexOf(this.regValue) === -1) return;
+        // this.xName = this.xName.split(this.regValue)[0];
+        // console.log(items)
+        this.readExcelColum(items, item);
+      });
+    });
+  }
+
+  // 正则表达式的方式获取excel的col 以及 row 的关键字
+  // [A,开始值Num,结束列名称,一共的行数]
+  getExcelProp(beginToEnd) {
+    
+    let beArr = beginToEnd.split(":");
+    let prop = [];
+    beArr.forEach(item => {
+      let regEn = /[a-zA-Z]+/;
+      let regNum = /[1-9][0-9]*/;
+      
+      prop.push(regEn.exec(item)[0], regNum.exec(item)[0]);
+    });
+    return prop;
+  }
+
+  // 自动填充横坐标（列A～AAB）
+  fillColumKey(ExcelProp) {
+    let end = ExcelProp[2];
+    let fillColum = [];
+    let regEn = /[a-zA-Z]+/;
+    for (let i in this.worksheet) {
+      if (i.indexOf(end) !== -1) {
+        fillColum.push(regEn.exec(i)[0]);
+        break;
+      }
+
+      fillColum.push(regEn.exec(i)[0]);
+    }
+    fillColum.shift(0);
+    return {
+      layoutX: fillColum,
+      layoutY: [ExcelProp[1], ExcelProp[3]]
+    };
+  }
+  // 读取某个列的指定数据
+  readExcelColumKey(key, sheetName) {
+    var worksheet = this.workbook.Sheets[sheetName];
+    var desiredCell = worksheet[key];
+    return desiredCell ? desiredCell.v : null;
+  }
+  // 循环某一列的所有数据并且读取
+  readExcelColum(key, sheetName) {
+    let mapKey = {};
+    if (key === this.keycell) return;
+    for (let i = 2; i < Number(this.fillObj.layoutY[1]) + 1; i++) {
+      let value = this.readExcelColumKey(key + i, sheetName);
+      let i18key = this.readExcelColumKey(this.keycell + i, sheetName);
+      if (value && i18key) mapKey[i18key] = value;
+    }
+    this.appendJson(`${key}1`, mapKey);
+  }
+  // 写入JSON读取添加
+  appendJson(name, dataJson) {
+    try {
+      let data = fs.readFileSync(
+        `${this.outfilePath}/${this.xName}.${this.filetype}`
+      );
+      let resData = JSON.parse(data);
+      // console.log("读取文件成功："+JSON.parse(data))
+      // console.log(JSON.stringify(Object.assign(JSON.parse(data),dataJson)))
+      // 直接执行默认去重复
+      let redArr = Object.keys(resData);
+      for (let i in dataJson) {
+        if (redArr.includes(i)) {
+          // console.log(i);
+          if (!this.repeatkey.includes(i)) {
+            this.repeatkey.push(i);
+          }
+        }
+      }
+      this.writeJson(name, Object.assign(JSON.parse(data), dataJson));
+    } catch (err) {
+      console.log("读取文件失败：" + err.message);
+      this.writeJson(name, dataJson);
+    }
+  }
+  writeJson(name, dataJson) {
+    try {
+      fs.writeFileSync(
+        `${this.outfilePath}/${this.xName}.${this.filetype}`,
+        JSON.stringify(dataJson, null, 2),
+        "utf8"
+      );
+    } catch (err) {
+      if (err) throw err;
+    }
+    // console.log("done");
+  }
+  checkSheet(sheetArr, isusereg, keyArr, keyvalue, filetype) {
+    this.repeatkey = [];
+    if (filetype) {
+      this.filetype = filetype;
+    }
+    let keyArrs = keyArr ? keyArr.split(",") : this.keycell.split(",");
+    console.log(keyArrs)
+    keyArrs.forEach(itemkey => {
+      this.keycell = itemkey;
+      sheetArr.forEach(item => {
+        this.worksheet = this.workbook.Sheets[item];
+        this.ExcelProp = this.getExcelProp(this.worksheet["!ref"]);
+        console.log(this.ExcelProp,this.worksheet["!ref"])
+        this.fillObj = this.fillColumKey(this.ExcelProp);
+        this.fillObj.layoutX.forEach(items => {
+          this.xName = this.readExcelColumKey(items + 1, item);
+          console.log( this.xName)
+          // if (this.xName.indexOf(this.regValue) === -1 && isusereg) return;
+          // if (isusereg) this.xName = this.xName.split(this.regValue)[0];
+          this.readExcelColum(items, item);
+        });
+        // columKeyArr.forEach(items => {
+        //   this.readExcelColum(items, item);
+        // });
+      });
+    });
+  }
+}
+export { FileClass };
+// let ff = new FileClassJson(
+//   "/Users/chenlei/Desktop/翻译.xlsx",
+//   "/Users/chenlei/Desktop/tt",
+//   "B"
+// );
+// ff.checkSheet(["翻译"], ["C", "D"]);
+// ff.checkAllSheet()
